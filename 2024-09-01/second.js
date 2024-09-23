64f63bbe4e459fb18bf209d6df2d37e3c675d940
Karmen Klaasen <146342698+kklaasen1@users.noreply.github.com>
2024-09-23 08:10:19 +0300

    feat: 2024-09-09 lesson done

--- 2024-09-01/second.js
+++ 2024-09-01/second.js
@@ -52,3 +52,59 @@ const greet = (name = "World") => 'Hello ${name}';
 
 console.log(greet());
 console.log(greet("Karmen"));
+
+
+
+// 11. Ülesanne: filtreerida välja uuesti massiivist kõik elemendid, mis on suuremad kui 4
+
+const array = [1, 2, 3, 4, 5, 6]
+
+const filteredArray = array.filter((element) => element > 4);
+
+console.log({ filteredArray });
+
+// 12. Ülesanne: luua nimede massiivi põhjal objektide massiiv, mis koosneb sellisel kujul objektidest: 
+
+const names = ["Anni", "Mari", "Mati", "Juku"];
+
+const objectifiedNames = names.map(name => {
+    return {
+        name: name,
+        age: name.length + 20,
+        email: '${name}@company.com',
+        address: '${name} Street 55',
+        username: name.split("").reverse().join(""),
+    };
+});
+
+//13. Ülesanne: Tahame juurde lisada pikkuse ja jätta eelnevad kõik andmed samaks, spread syntax
+
+let karmen = {
+    name: "Karmen",
+    school: "TLÜ",
+};
+
+karmen = {...karmen, height: 160 };
+
+console.log({ karmen });
+
+// const ei lase väärtust muuta
+
+// 14: Ülesanne: == / ===
+
+console.log(1 == "1");
+console.log(1 === "1");
+
+// async/await Promise
+
+const myPromise = () => {
+    return new Promise((resolve) => setTimeout(() => resolve("done"), 1000));
+};
+
+const runPromise = async () => {
+    console.log(await myPromise());
+};
+
+runPromise();
+
+// setTimeout(() => console.log("timeout"), 2000);
